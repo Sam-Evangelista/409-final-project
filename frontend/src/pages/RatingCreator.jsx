31517723281063d50638d2facd190df556a93eab
Maya <mayavajit@gmail.com>
2025-12-04 20:49:49 -0600

    Started RatingCreator

---
+++ frontend/src/pages/RatingCreator.jsx
@@ -0,0 +1,16 @@
+import '../assets/RatingCreator.css';
+
+function RatingCreator() {
+    return (
+        <div className="rating-creator-page">
+            <div className="ratings-header">
+                {/* <div> */}
+                    <img className='user' src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png"/>            
+                    <h1>Your Ratings</h1>
+                {/* </div> */}
+            </div>
+        </div>
+    );
+}
+
+export default RatingCreator;
