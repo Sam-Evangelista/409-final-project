4784d425996611ac569d1776bfff63997b1bccb3
scuruchima1 <scuruchima1@gmail.com>
2025-12-08 21:33:45 -0600

    added most_listened_albums to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -29,6 +29,10 @@ const userSchema = new mongoose.Schema({
         type: [String],
         default: []
     },
+    most_listened_albums: {
+        type: [String],
+        default: []
+    },
     bio : {
         type: String,
         default: ''
